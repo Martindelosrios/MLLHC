06133ab29d565b4e0e1d099de3f886a709cf8f5b
Martin de los Rios <martindelosrios13@gmail.com>
2025-02-14 13:14:19 +0100

    code for downloading data from ro

---
+++ codes/scp.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+# run with bash scp.bash
+
+# Definir el usuario y la dirección IP del servidor remoto
+usuario=ro
+direccion_ip=150.244.221.192
+
+# Definir el directorio de origen en el servidor remoto
+directorio_origen=/home/ro/MG5_aMC_v2_9_9/pptox1n23j/
+
+# Definir el directorio de destino en la máquina local
+directorio_destino=/home/martin/martin/trabajos/MLLHC/data/BP8/
+
+# Establecer la conexión maestra SSH
+ssh -o ControlMaster=yes -o ControlPath=/tmp/ssh-control-$usuario@$direccion_ip -Nf $usuario@$direccion_ip
+
+# Bucle para transferir archivos del servidor remoto a la máquina local
+for ((linea=1; linea<=10; linea++)); do
+    archivo_origen="BP8_run100k_$linea.lhco"
+    scp -o ControlPath=/tmp/ssh-control-$usuario@$direccion_ip $usuario@$direccion_ip:$directorio_origen$archivo_origen $directorio_destino
+done
+
+# Cerrar la conexión maestra SSH después de la transferencia de archivos
+ssh -o ControlPath=/tmp/ssh-control-$usuario@$direccion_ip -O exit $usuario@$direccion_ip
+
