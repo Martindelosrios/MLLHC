339570b179c43af4651c2a91c2cf57983545ea00
martindelosrios <martindelosrios13@gmail.com>
2025-05-22 16:28:54 +0200

    start the analysis of the lhco files

--- codes/scp.bash
+++ codes/scp.bash
@@ -6,20 +6,21 @@ usuario=ro
 direccion_ip=150.244.221.199
 
 # Definir el directorio de origen en el servidor remoto
-#directorio_origen=/home/ro/MG5_aMC_v2_9_9/pptox1n2j_nmssm/
-directorio_origen=/home/ro/MG5_aMC_v2_9_9/pptox1n3j_nmssm/
+directorio_origen2=/home/ro/MG5_aMC_v2_9_9/pptox1n2j_nmssm/
+directorio_origen3=/home/ro/MG5_aMC_v2_9_9/pptox1n3j_nmssm/
 
 # Definir el directorio de destino en la máquina local
-#directorio_destino=/home/martinrios/martin/trabajos/MLLHC/data/BP1_1/pptox1n2j_nmssm/
-directorio_destino=/home/martinrios/martin/trabajos/MLLHC/data/BP1_1/pptox1n3j_nmssm/
+directorio_destino2=/home/martinrios/martin/trabajos/MLLHC/data/BP8/pptox1n2j_nmssm/
+directorio_destino3=/home/martinrios/martin/trabajos/MLLHC/data/BP8/pptox1n3j_nmssm/
 
 # Establecer la conexión maestra SSH
 ssh -o ControlMaster=yes -o ControlPath=/tmp/ssh-control-$usuario@$direccion_ip -Nf $usuario@$direccion_ip
 
 # Bucle para transferir archivos del servidor remoto a la máquina local
-for ((linea=1; linea<=10; linea++)); do
-    archivo_origen="BP1_1_run100k_$linea.lhco"
-    scp -o ControlPath=/tmp/ssh-control-$usuario@$direccion_ip $usuario@$direccion_ip:$directorio_origen$archivo_origen $directorio_destino
+for ((linea=1; linea<=4; linea++)); do
+    archivo_origen="BP8_"$linea"_run100k_1.lhco"
+    scp -o ControlPath=/tmp/ssh-control-$usuario@$direccion_ip $usuario@$direccion_ip:$directorio_origen2$archivo_origen $directorio_destino2
+    scp -o ControlPath=/tmp/ssh-control-$usuario@$direccion_ip $usuario@$direccion_ip:$directorio_origen3$archivo_origen $directorio_destino3
 done
 
 # Cerrar la conexión maestra SSH después de la transferencia de archivos
